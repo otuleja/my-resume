25cd181d5b1fa780f04acc3812ec9e0eba2a8255
Owen Tuleja <otuleja@Owens-MacBook-Pro.local>
2023-10-11 13:09:36 +0200

    bring back react views

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const port = process.env.PORT || 3000;
-app.get("/", async function (req, res) {
+app.get("/health", async function (req, res) {
   res.send("Hello world")
 })
 
-// app.get("/", async function (req, res) {
-//   res.render('index', { name: 'Johnnnn' });
-// })
+app.get("/", async function (req, res) {
+  res.render('index', { name: 'Johnnnn' });
+})
 
 // app.get("/about", async function (req, res) {
 //   res.render('about', { name: 'Owen' });
